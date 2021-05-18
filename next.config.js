5d427d0eb94428a95d5f8c6e8ab337de6b16bfe0
Pablo P Varela <pablo@pablopunk.com>
2021-05-18 19:37:04 +0200

    Add a rewrite for fonts (fixes safari on iOS)

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ module.exports = {
       {
         source: '/count.js',
         destination: 'https://gc.zgo.at/count.js'
+      },
+      {
+        source: '/fonts',
+        destination:
+          'https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap'
       }
     ]
   }
