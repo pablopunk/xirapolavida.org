820d44a076ecaeae0ddc9f0941758900d48b4306
Pablo P Varela <pablo@pablopunk.com>
2021-06-24 13:18:53 +0200

    Use preact in production

--- next.config.js
+++ next.config.js
@@ -14,11 +14,20 @@ module.exports = {
       },
     ]
   },
-  webpack(config) {
+  webpack(config, { dev, isServer }) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
+
+    // use Preact in production
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
 
     return config
   },
