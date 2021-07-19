aab21f1f63df52e1b61d9618f1a0aeedd97f227f
Pablo P Varela <pablo@pablopunk.com>
2021-07-20 00:10:56 +0200

    404 and fix header
    * fix image header widh
    * 404 error pages are now statically optimized

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+function Error404() {
+  return (
+    <div className="flex items-center h-full text-6xl mt-[20vh] text-danger">
+      <p>Erro 404</p>
+    </div>
+  )
+}
+
+export default Error404
